cf9d2efcdf2b8360e270b6b8b5213cb938976a04
Daniel Schömer <daniel.schoemer@gmx.net>
2008-11-02 16:06:06 +0100

    Whitespace cleanup.

--- lib/on_zero_load/idle/xscreensaver.rb
+++ lib/on_zero_load/idle/xscreensaver.rb
@@ -16,17 +16,17 @@ module OnZeroLoad
             VALUE idle_time() {
               static Display   *display;
               XScreenSaverInfo *info = XScreenSaverAllocInfo();
-    
+
               if (!display) {
                 display = XOpenDisplay(0);
               }
-    
+
               XScreenSaverQueryInfo(display, DefaultRootWindow(display), info);
-    
+
               return INT2NUM(info->idle);
             }
           }
-          
+
           builder.c %{
             VALUE dpms_state() {
               static Display *display;
@@ -37,18 +37,18 @@ module OnZeroLoad
               VALUE  rb_hash;
               VALUE  rb_hash_timeout;
               VALUE  rb_state;
-      
+
               if (!display) {
                 display = XOpenDisplay(0);
               }
-      
+
               if (DPMSQueryExtension(display, &dummy, &dummy)) {
                 if (DPMSCapable(display)) {
                   DPMSGetTimeouts(display, &standby, &suspend, &off);
                   DPMSInfo(display, &state, &on_off);
-                } 
+                }
               }
-      
+
               switch (state) {
                 case DPMSModeStandby: {
                   rb_state = ID2SYM(rb_intern("standby"));
@@ -68,26 +68,26 @@ module OnZeroLoad
                   break;
                 }
               }
-    
+
               rb_hash         = rb_hash_new();
               rb_hash_timeout = rb_hash_new();
-    
+
               rb_hash_aset(rb_hash, ID2SYM(rb_intern("state")), rb_state);
               rb_hash_aset(rb_hash, ID2SYM(rb_intern("timeout")), rb_hash_timeout);
-    
+
               rb_hash_aset(rb_hash_timeout, ID2SYM(rb_intern("standby")),
                                             INT2NUM(standby));
               rb_hash_aset(rb_hash_timeout, ID2SYM(rb_intern("suspend")),
                                             INT2NUM(suspend));
               rb_hash_aset(rb_hash_timeout, ID2SYM(rb_intern("off")),
                                             INT2NUM(off));
-    
+
               return rb_hash;
             }
           }
         end
       end
-      
+
       def self.total_idle_time(dpms_state = self.dpms_state)
         diff = {
           :on      =>  0,
@@ -98,7 +98,7 @@ module OnZeroLoad
                        dpms_state[:timeout][:suspend] +
                        dpms_state[:timeout][:off]) * 1000,
         }
-    
+
         self.idle_time + diff[dpms_state[:state]]
       end
     end
