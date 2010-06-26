3a3ff23465905836e4f3e4ba831b19e112629409
Daniel Schömer <daniel.schoemer@gmx.net>
2010-06-26 11:02:56 +0200

    Reduced OnZeroLoad::LoadAvg class to the class methods current_raw() and current(). There is no need for more logic at this stage.

--- lib/on_zero_load/loadavg.rb
+++ lib/on_zero_load/loadavg.rb
@@ -1,164 +1,26 @@
 module OnZeroLoad
-  # Objects of this class represent the average load (loadavg) of a Linux system.
-  #
-  # The loadavg can be obtained from the file <code>/proc/loadavg</code>. See <code>man 5
-  # proc</code> for detailed description of that file.
   class LoadAvg
-    include Comparable
-
-    # The load averaged over the last minute. Negative numbers are rejected by raising a
-    # +ArgumentExecption+. See <code>#validate</code>.
-    attr :one
-
-    # The load averaged over the five minutes. Negative numbers are rejected by raising a
-    # +ArgumentExecption+. See <code>#validate</code>.
-    attr :five
-
-    # The load averaged over the fifteen minutes. Negative numbers are rejected by raising
-    # a +ArgumentExecption+. See <code>#validate</code>.
-    attr :fifteen
-
-    # Holds the available attribute names to simplify some methods.
-    LOADS = [ :one, :five, :fifteen ].freeze
-
-    # Create a new instance and set the loadavg values.
-    #
-    # Accepted parameter-keywords are <code>:one</code>, <code>:five</code> and
-    # <code>:fifteen</code>.  They must hold +nil+'s or positive numbers.  See
-    # <code>#validate</code>.
-    def initialize(params = {})
-      LOADS.each do |load|
-        self.send("#{load}=".to_sym, params[load])
-      end
-    end
-
-    def one=(one) # :nodoc:
-      validate(one)
-      @one = one
-    end
-
-    def five=(five) # :nodoc:
-      validate(five)
-      @five = five
-    end
-
-    def fifteen=(fifteen) # :nodoc:
-      validate(fifteen)
-      @fifteen = fifteen
-    end
-
-    # Tests if +value+ is +nil+ or a positive number. Raises an +ArgumentException+ if
-    # not.
-    def validate(value)
-      unless (value.nil? || (value.kind_of?(Numeric) && 0 <= value))
-        raise ArgumentError.new("Loadavg values must be positive, not #{value}.")
-      end
-    end
-
-    # Computes a hash-value. Creates an array of the loadavg values and returns the
-    # array's hash-value.
-    def hash
-      LOADS.map { |load|
-        self.send(load)
-      }.hash
-    end
-
-    # Tests this instance for equality to another object.  Both are considered equal, if
-    # +other+ responds to <code>one</code>, <code>five</code> and <code>fifteen</code>,
-    # and returns values equal to those of this instance.
-    def == other
-      self.equal?(other) ||
-      LOADS.all? { |load|
-        other.respond_to?(load) && self.send(load) == other.send(load)
-      }
-    end
-
-    alias_method(:eql?, :==)
-
-    # Compares this instance to another object.  The comparison takes into account only
-    # the non-+nil+ loadavg values of both objects.  Returns -1 if this instance is
-    # considered smaller than +other+, 0 if both are considered equal, and 1 if this
-    # instance is considered larger than +other+.
-    #
-    # FIXME (describe <=> algorithm in detail)
-    def <=> other
-      comparisons = LOADS.map { |load|
-        a = self.send(load)
-        b = other.respond_to?(load) && other.send(load)
-
-        a <=> b if a && b
-      }.compact
-
-      if comparisons.empty?
-        nil
-      else
-        comparisons.find { |comparison| 0 != comparison } || 0
-      end
-    end
-
-    # Returns a string representation of this instance.
-    def to_s
-      "load:" + LOADS.map { |load|
-        "#{self.send(load)}:#{load}" if self.send(load)
-      }.compact.join(",")
-    end
-
-    alias_method(:inspect, :to_s)
-
-    # Parses +string+ to a +LoadAvg+ instance.
-    #
-    # Scans +string+ for substrings that look like a valid loadavg specification. Only the
-    # last value for the one-, five- and fifteen-minute loadavg is stored in the retured
-    # object.
-    #
-    # A valid loadavg specification looks like this: (EBNF)
-    #
-    #  loadavg = number | number ":" minutes
-    #  number  = [ sign ] digits [ "." digits ] [ "e" [ sign ] digits ]
-    #  minutes = "one" | "five" | "fifteen" | "1" | "5" | "15"
-    #  sign    = "+" | "-"
-    #  digits  = digit { digit }
-    #  digit   = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9"
-    #
-    # A loadavg specification without +minutes+ is assumed to be a one-minute loadavg.
-    def self.parse(string)
-      num    = '[+-]?(?:\d*\.\d+|\d+)(?:e[+-]?\d+)?'
-      load   = '\b(one|five|fifteen|1|5|15)\b'
-      expr   = Regexp.new("(#{num})(?::(#{load}))?")
-      sub    = { "1" => "one", "5" => "five", "15" => "fifteen" }
-      params = {}
-
-      string.to_s.scan(expr).map do |value, load|
-        value  = value.to_f
-        load ||= "one"
-
-        sub.each do |num, str|
-          load.sub!(Regexp.new("^#{num}$"), str)
-        end
-
-        params[load.to_sym] = value
-      end
-
-      self.new(params)
-    end
-
     # The current loadavg values from <code>/proc/loadavg</code> as three-elemental array
     # of floats.
     #
     #  [0.16, 0.14, 0.11]
     def self.current_raw
-      open("/proc/loadavg") { |f| f.read(14) } .split.map { |s| s.to_f }
+      data = open("/proc/loadavg") { |f| f.readline } .split[0..2]
+      
+      data.map { |field|
+        /^[[:digit:].,]+$/ =~ field ? field.to_f : field
+      }
     end
 
-    # The current loadavg.
+    # The current loadavg as a hash.
     #
-    #  load:0.16:one,0.14:five,0.11:fifteen
+    #  { :one => 0.16, :five => 0.14, :fifteen => 0.11 }
     def self.current
       data = self.current_raw
 
-      self.new({ :one     => data[0],
-                 :five    => data[1],
-                 :fifteen => data[2] })
+      { :one     => data[0],
+        :five    => data[1],
+        :fifteen => data[2] }
     end
   end
 end
--- spec/on_zero_load/load_spec.rb
+++ spec/on_zero_load/load_spec.rb
@@ -34,154 +34,27 @@ module OnZeroLoad
     end
 
     it "includes values for one, five and fifteen minute load" do
-      @load.should respond_to(:one)
-      @load.should respond_to(:five)
-      @load.should respond_to(:fifteen)
+      @load[:one].should_not be_nil
+      @load[:five].should_not be_nil
+      @load[:fifteen].should_not be_nil
     end
 
     it "includes only numeric values" do
-      @load.one.should     be_kind_of(Numeric)
-      @load.five.should    be_kind_of(Numeric)
-      @load.fifteen.should be_kind_of(Numeric)
+      @load[:one].should     be_kind_of(Numeric)
+      @load[:five].should    be_kind_of(Numeric)
+      @load[:fifteen].should be_kind_of(Numeric)
     end
 
     it "includes the raw data value for one minute load" do
-      @load.one.should == @raw[0]
+      @load[:one].should == @raw[0]
     end
 
     it "includes the raw data value for five minute load" do
-      @load.five.should == @raw[1]
+      @load[:five].should == @raw[1]
     end
 
     it "includes the raw data value for fifteen minute load" do
-      @load.fifteen.should == @raw[2]
-    end
-  end
-
-
-  describe "LoadAverage object" do
-    def create(*params)
-      LoadAvg.new(*params)
-    end
-
-    before do
-      @empty = create()
-    end
-
-    it "stores one minute loadavg" do
-      @empty.one.should be_nil
-      @empty.one = 0.1
-      @empty.one.should == 0.1
-    end
-
-    it "stores five minute loadavg" do
-      @empty.five.should be_nil
-      @empty.five = 0.2
-      @empty.five.should == 0.2
-    end
-
-    it "stores fifteen minute loadavg" do
-      @empty.fifteen.should be_nil
-      @empty.fifteen = 0.3
-      @empty.fifteen.should == 0.3
-    end
-
-    it "accepts only positive values" do
-      lambda { @empty.one     = -1 } .should raise_error(ArgumentError)
-      lambda { @empty.five    = -1 } .should raise_error(ArgumentError)
-      lambda { @empty.fifteen = -1 } .should raise_error(ArgumentError)
-    end
-
-    it "has an initializer with named parameters" do
-      p = { :one => 0.3, :five => 0.4, :fifteen => 0.5 }
-      a = create(:one     => p[:one])
-      b = create(:five    => p[:five])
-      c = create(:fifteen => p[:fifteen])
-      d = create(p)
-
-      a.one.should     == p[:one]
-      a.five.should    be_nil
-      a.fifteen.should be_nil
-
-      b.one.should     be_nil
-      b.five.should    == p[:five]
-      b.fifteen.should be_nil
-
-      c.one.should     be_nil
-      c.five.should    be_nil
-      c.fifteen.should == p[:fifteen]
-
-      d.one.should     == p[:one]
-      d.five.should    == p[:five]
-      d.fifteen.should == p[:fifteen]
-    end
-
-    it "has equivalent == and eql? methods" do
-      @empty.should equal(@empty)
-      @empty.should eql(@empty)
-      (@empty.eql? @empty).should == (@empty == @empty)
-
-      a = create()
-      b = create(:one => 0.6, :five => 0.7, :fifteen => 0.8)
-
-      @empty.should_not equal(a)
-      @empty.should eql(a)
-      @empty.should == a
-      a.should_not equal(@empty)
-      a.should eql(@empty)
-      (a.eql? @empty).should == (a == @empty)
-
-      a.one = b.one
-      a.should_not eql(b) ; (a.eql? b).should == (a == b)
-      b.should_not eql(a) ; (b.eql? a).should == (b == a)
-
-      a.five = b.five
-      a.should_not eql(b) ; (a.eql? b).should == (a == b)
-      b.should_not eql(a) ; (b.eql? a).should == (b == a)
-
-      a.fifteen = b.fifteen
-      a.should eql(b) ; (a.eql? b).should == (a == b)
-      b.should eql(a) ; (b.eql? a).should == (b == a)
-    end
-
-    it "has == and hash, such that a == b implies a.hash == b.hash" do
-      a = create()
-      b = create(:one => 0.6, :five => 0.7, :fifteen => 0.8)
-      c = create(:one => 0.6, :five => 0.7, :fifteen => 0.8)
-
-      (a == b).should == (a.hash == b.hash)
-      (b == a).should == (b.hash == a.hash)
-      (b == c).should == (b.hash == c.hash)
-      (c == b).should == (c.hash == b.hash)
-    end
-
-    it "is comparable to others"
-    it "allows comparing incomplete instances"
-
-    it "can be parsed from string" do
-      {
-        ""            => {},
-        "1"           => { :one => 1 },
-        "0.1"         => { :one => 0.1 },
-        "0.1,0.2"     => { :one => 0.2 },
-        "1:one"       => { :one => 1 },
-        "1:1"         => { :one => 1 },
-        "0.1:one"     => { :one => 0.1 },
-        "0.1:1"       => { :one => 0.1 },
-        "0.2:five"    => { :five => 0.2 },
-        "0.2:5"       => { :five => 0.2 },
-        "0.3:fifteen" => { :fifteen => 0.3 },
-        "0.3:15"      => { :fifteen => 0.3 },
-        "load:1:one,2:five,3:fifteen" => { :one => 1, :five => 2, :fifteen => 3 },
-        "load:1:1,2:5,3:15" => { :one => 1, :five => 2, :fifteen => 3 },
-        "a0.1a"     => { :one => 0.1 },
-        "a0.1:onea"     => { :one => 0.1 },
-      }.each do |string, params|
-        a = LoadAvg.parse(string)
-        b = create(params)
-
-        a.should eql(b)
-      end
+      @load[:fifteen].should == @raw[2]
     end
   end
 end
