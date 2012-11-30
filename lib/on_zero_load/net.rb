bdb3fb7d515aac39bce6e29b668d3b23a3035e1e
Daniel Schömer <daniel.schoemer@gmx.net>
2012-11-30 19:51:59 +0100

    Rewritten OnZeroLoad::Net to get data from the files in /sys/class/net/*/statistics/ instead of parsing /proc/net/dev. Results in more counters and more descriptive names.

--- lib/on_zero_load/net.rb
+++ lib/on_zero_load/net.rb
@@ -1,66 +1,61 @@
+require 'ruby-units'
+
 module OnZeroLoad
   class Net
-    # The current network interface statistics from <code>/proc/net/dev</code> as
-    # array. The file's lines are just split on whitespace and <code>/[-:|]/</code>. The
-    # numeric field values are converted to integers.
-    #
-    #  [["Inter", "Receive", "Transmit"],
-    #   ["face", "bytes", "packets", "errs", "drop", "fifo", "frame", "compressed", "multicast",
-    #            "bytes", "packets", "errs", "drop", "fifo", "colls", "carrier", "compressed"],
-    #   ["lo",   1062918186, 1571073, 0, 0, 0, 0, 0, 0,
-    #            1062918186, 1571073, 0, 0, 0, 0, 0, 0],
-    #   ["eth0", 1101824917,  834046, 0, 0, 0, 0, 0, 0,
-    #              66796500,  742011, 0, 0, 0, 0, 0, 0]]
-    def self.current_raw
-      open("/proc/net/dev") { |f| f.readlines } .map { |line|
-        line.strip.split(/[-:|[:space:]]+/).map { |field|
-          /^[[:digit:].,]+$/ =~ field ? field.to_i : field
-        }
-      }
-    end
-
     # The current network interface statistics as hash. The interface-name-keys are
     # strings; the counter-keys are symbols.
     #
-    #  { "lo"   => { :receive  => { :bytes => 1062918168, :packets => 1571073,
-    #                               :errs => 0, :drop => 0, :fifo => 0, :frame => 0,
-    #                               :compressed => 0, :multicast => 0 },
-    #                :transmit => { :bytes => 1062918168, :packets => 1571073,
-    #                               :errs => 0, :drop => 0, :fifo => 0, :colls => 0,
-    #                               :carrier => 0, :compressed => 0 } },
-    #    "eth0" => { :receive  => { :packets => 1101824917, :bytes => 834046,
-    #                               :errs => 0, :drop => 0, :fifo => 0, :frame => 0,
-    #                               :compressed => 0, :multicast => 0 },
-    #                :transmit => { :bytes => 66796500, :packets => 742011,
-    #                               :errs => 0, :drop => 0, :fifo => 0, :colls => 0,
-    #                               :carrier => 0, :compressed => 0 } } }
+    # Data is read from the files in <code>/sys/class/net/*/statistics/</code>.
+    #
+    #   { "lo"   => { :collisions => 0,
+    #                 :multicast  => 0,
+    #                 :receive    => { :bytes => 15891551,   :compressed => 0,
+    #                                  :crc_errors => 0,     :dropped => 0,
+    #                                  :errors => 0,         :fifo_errors => 0,
+    #                                  :frame_errors => 0,   :length_errors => 0,
+    #                                  :missed_errors => 0,  :over_errors => 0,
+    #                                  :packets => 232208 },
+    #                 :transmit   => { :aborted_errors => 0, :bytes => 15891551,
+    #                                  :carrier_errors => 0, :compressed => 0,
+    #                                  :dropped => 0,        :errors => 0,
+    #                                  :fifo_errors => 0,    :heartbeat_errors => 0,
+    #                                  :packets => 232208,   :window_errors => 0 } },
+    #     "eth0" => { :collisions => 0,
+    #                 :multicast  => 0,
+    #                 :receive    => { :bytes => 8220192299, :compressed => 0,
+    #                                  :crc_errors => 0,     :dropped => 37,
+    #                                  :errors => 0,         :fifo_errors => 0,
+    #                                  :frame_errors => 0,   :length_errors => 0,
+    #                                  :missed_errors => 0,  :over_errors => 0,
+    #                                  :packets => 5911129 },
+    #                 :transmit   => { :aborted_errors => 0, :bytes => 342981201,
+    #                                  :carrier_errors => 0, :compressed => 0,
+    #                                  :dropped => 0,        :errors => 0,
+    #                                  :fifo_errors => 0,    :heartbeat_errors => 0,
+    #                                  :packets => 3694591,  :window_errors => 0 } } }
     def self.current
       values = {}
-      data   = downcase_header_sym(self.current_raw)
 
-      data[2 .. data.size].each do |line|
-        name           = line.first
-        values[name] ||= {}
+      Dir.glob("/sys/class/net/*/statistics").each do |dir|
+        dev = File.basename(File.dirname(dir))
 
-        line[1 .. line.size].each_with_index do |field, index|
-          dir  = data[0][index / 8 + 1] # first header line, should be :receive or :transmit
-          type = data[1][index + 1]     # second header line, should be field name
+        Dir.entries(dir).reject { |e| e =~ /^\.+/ } .sort.each do |file|
+          key   = File.basename(file)
+                      .downcase
+                      .sub(/^tx_/, "transmit_")
+                      .sub(/^rx_/, "receive_")
+                      .split("_", 2)
+                      .map(&:to_sym)
+          value = File.read(File.join(dir, file)).to_i
 
-          values[name][dir]     ||= {}
-          values[name][dir][type] = field
+          case key.size
+            when 1 then  (values[dev] ||= {})[key.first] = value
+            when 2 then ((values[dev] ||= {})[key.first] ||= {})[key.last] = value
+          end
         end
       end
 
       values
-    end
-
-    # TODO (comment downcase_header_sym)
-    def self.downcase_header_sym(data)
-      [0..1].each do |n| # convert headers to downcase symbols
-        data[n] = data[n].map { |l| l.map { |f| f.downcase.to_sym } }
-      end
-
-      data
     end
   end
 end
--- spec/on_zero_load/net_spec.rb
+++ spec/on_zero_load/net_spec.rb
@@ -3,60 +3,9 @@ require 'on_zero_load/net'
 
 
 module OnZeroLoad
-  describe "System monitor network raw data" do
-    before do
-      @raw = Net.current_raw
-    end
-
-    it "is a non-empty array" do
-      @raw.should be_kind_of(Array)
-      @raw.should_not be_empty
-    end
-
-    it "is a 2-dimensional array" do
-      @raw.each do |a|
-        a.should be_kind_of(Array)
-      end
-    end
-
-    it "contains headers in the first two sub-arrays" do
-      @raw.size.should >= 2
-      @raw[0..1].each do |a|
-        a.should_not be_empty
-        a.each do |b|
-          b.should be_kind_of(String)
-        end
-      end
-    end
-
-    it "contains interface name and 16 numbers for each interface" do
-      @raw[2 .. @raw.size].each do |a|
-        a.first.should be_kind_of(String)
-        a.size.should == 17
-        a[1 .. a.size].each do |n|
-          n.should be_kind_of(Numeric)
-        end
-      end
-    end
-  end
-
-
   describe "System monitor network data" do
     before do
-      @raw = Net.current_raw
       @net = Net.current
-    end
-
-    it "is a non-empty hash" do
-      @net.should be_kind_of(Hash)
-      @net.should_not be_empty
-    end
-
-    it "contains the interfaces from raw data" do
-      raw_names = @raw[2 .. @raw.size].map {|a| a.first }.sort
-      names     = @net.keys.sort
-
-      names.should == raw_names
     end
 
     it "contains hashes for receive and transmit counters per interface" do
@@ -71,9 +20,9 @@ module OnZeroLoad
 
     it "contains counters per interface addressed by counter-name-symbols" do
       @net.each_pair do |name, values|
-        values.each_pair do |direction, counters|
-          counters.should_not be_empty
-          counters.keys.each do |counter_name|
+        [:receive, :transmit].each do |direction|
+          values[direction].should_not be_empty
+          values[direction].keys.each do |counter_name|
             counter_name.should be_kind_of(Symbol)
           end
         end
@@ -82,25 +31,42 @@ module OnZeroLoad
 
     it "contains numbers as counter values" do
       @net.each_pair do |name, values|
-        values.each_pair do |direction, counters|
-          counters.each_pair do |counter_name, value|
+        [:receive, :transmit].each do |direction|
+          values[direction].each_pair do |counter_name, value|
             value.should be_kind_of(Numeric)
           end
         end
       end
     end
 
-    it "contains a defined set of receive and transmit counters per interface" do
+    it "contains a defined set of counters per interface" do
+      dev_keys = [ :collisions, :multicast, :receive, :transmit ]
+      rx_keys  = [ :bytes,
+                   :compressed,
+                   :crc_errors,
+                   :dropped,
+                   :errors,
+                   :fifo_errors,
+                   :frame_errors,
+                   :length_errors,
+                   :missed_errors,
+                   :over_errors,
+                   :packets ]
+      tx_keys  = [ :aborted_errors,
+                   :bytes,
+                   :carrier_errors,
+                   :compressed,
+                   :dropped,
+                   :errors,
+                   :fifo_errors,
+                   :heartbeat_errors,
+                   :packets,
+                   :window_errors ]
+      
       @net.each_pair do |name, values|
-        [:bytes, :packets, :errs, :drop, :fifo, :frame,
-         :compressed, :multicast].each do |c|
-          values[:receive].keys.should include(c)
-        end
-
-        [:bytes, :packets, :errs, :drop, :fifo, :colls,
-         :carrier, :compressed].each do |c|
-          values[:transmit].keys.should include(c)
-        end
+        values.keys.should include(*dev_keys)
+        values[:receive].keys.should include(*rx_keys)
+        values[:transmit].keys.should include(*tx_keys)
       end
     end
   end
