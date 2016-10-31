4c51ea603d869690ebcb9e75bce25f771919e8aa
Daniel Schömer <daniel.schoemer@gmx.net>
2016-10-31 21:48:20 +0100

    Added utf8 encoding and frozen string literals

--- features/steps/steps_helper.rb
+++ features/steps/steps_helper.rb
@@ -1,4 +1,7 @@
-$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__),
-                                              '..', '..', 'lib')))
+# -*- coding: utf-8; -*-
+# frozen_string_literal: true
+# vim:set fileencoding=utf-8:
+
+$LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'lib')))
 
 require 'on_zero_load'
